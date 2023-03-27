4dafdf564daf74529c2b48a1cda20faf8d06b36f
gogolashvili <ggogolashvili91@gmail.com>
2023-03-27 08:40:57 +0400

    add header

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import moon from "../images/moon.png";
+import { Outlet } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <>
+      <header>
+        <div className="headerDiv">
+          <h1 className="title">Where in the world?</h1>
+          <div className="modeDiv">
+            <img className="moonImg" src={moon} alt="moon" />
+            <p className="modeText">Dark Mode</p>
+          </div>
+        </div>
+      </header>
+      <Outlet />
+    </>
+  );
+}
